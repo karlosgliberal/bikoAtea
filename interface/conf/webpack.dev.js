ee74bf1bb53d33d65e5f0ea1442f6e2f7ab186c9
Aitor Resano <aitor.rl@gmail.com>
2018-05-14 16:17:26 +0200

    Interface

---
+++ interface/conf/webpack.dev.js
@@ -0,0 +1,21 @@
+const webpack = require('webpack');
+const path = require('path');
+const Merge = require('webpack-merge');
+const CommonConfig = require('./webpack.common.js');
+
+module.exports = Merge(CommonConfig, {
+  plugins: [
+      new webpack.HotModuleReplacementPlugin(),
+      new webpack.NamedModulesPlugin()
+  ],
+  devServer: {
+    publicPath: '/',
+    port: 9000,
+    contentBase: path.join(process.cwd(), 'dist'), // static file location
+    host: 'localhost',
+    historyApiFallback: true, // true for index.html upon 404, object for multiple paths
+    noInfo: false,
+    stats: 'minimal',
+    hot: true  // hot module replacement. Depends on HotModuleReplacementPlugin
+  }
+});
---
+++ interface/src/image.js
@@ -0,0 +1,24 @@
+import bikoSvg from './images/logo-biko.svg';
+import twitterSvg from './images/twitter.svg';
+import facebookSvg from './images/facebook.svg';
+import whatSvg from './images/what.svg';
+import footerSvg from './images/footer-logos.svg';
+import puertaSvg from './images/puerta.svg';
+
+var logoBiko = document.getElementById('logo-biko');
+logoBiko.src = bikoSvg;
+
+var logoTwitter = document.getElementById('logo-twitter');
+logoTwitter.src = twitterSvg;
+
+var logoFacebook = document.getElementById('logo-facebook');
+logoFacebook.src = facebookSvg;
+
+var logoWhat= document.getElementById('logo-what');
+logoWhat.src = whatSvg;
+
+var logoFooter= document.getElementById('logo-footer');
+logoFooter.src = footerSvg;
+
+var logoPuerta = document.getElementById('logo-puerta');
+logoPuerta.src = puertaSvg;
