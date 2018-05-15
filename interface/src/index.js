4bdf410483702e75aaf7246c70c83c890fdec703
karlos g liberal <karlosgliberal@gmail.com>
2018-05-15 10:40:19 +0200

    botones en la interfaz

--- interface/src/index.js
+++ interface/src/index.js
@@ -1,85 +1,11 @@
-import firebase from '@firebase/app';
-import '@firebase/firestore';
-import 'image.js';
+import 'peticionParticle';
 import './scss/style.scss';
-import React from 'react';
-import ReactDOM from 'react-dom';
-import 'd3Skech.js';
+import 'image.js';
 
-// const coleccionDatosGenericos = 'datosGenericos';
-// const documentosDatosGenericos = 'datos';
+var element = document.getElementById('azul');
 
-var config = {
-  apiKey: 'AIzaSyA1W0B66TglY86WGLzAckB7K-XT8SxxVNk',
-  authDomain: 'puertabiko.firebaseapp.com',
-  databaseURL: 'https://puertabiko.firebaseio.com',
-  projectId: 'puertabiko',
-  storageBucket: 'puertabiko.appspot.com',
-  messagingSenderId: '683112733184'
-};
-
-firebase.initializeApp(config);
-const firestore = firebase.firestore();
-const settings = { /* your settings... */ timestampsInSnapshots: true };
-firestore.settings(settings);
-const db = firebase.firestore();
-
-class Valor extends React.Component {
-  render() {
-    return React.createElement('span', null, `${this.props.valor}`);
-  }
+function movida(){
+  console.log('movida');
 }
 
-const datosGenericosPuerta = () => {
-  const refDatosGenericos = db.collection('datosGenericos').doc('datos');
-  refDatosGenericos.onSnapshot(function(doc) {
-    let datosTotales = doc.data();
-    render(datosTotales.aperturasDiarias, 'puertaHoy');
-  });
-};
-datosGenericosPuerta();
-
-const valoresTotales = tipo => {
-  const refDatos = db.collection('puertaBiko');
-  return refDatos
-    .orderBy(tipo, 'desc')
-    .limit(1)
-    .get()
-    .then(({ docs }) => {
-      return docs;
-    });
-};
-
-const testIsma = () => {
-  db
-    .collection('puertaBiko')
-    .where('timestamp', '>', '24-04-2018T16:00:00')
-    .where('timestamp', '<', '24-04-2018T20:00:00')
-    .get()
-    .then(({ docs }) => {
-      docs.map(doc => {
-          console.log(doc);
-      });
-    });
-};
-testIsma();
-
-Promise.all([valoresTotales('sonido'), valoresTotales('temperatura')]).then(
-  values => {
-    let valoresResSonido = values[0][0].data();
-    let valoresResTemp = values[1][0].data();
-
-    render(valoresResSonido.sonido, 'sonidoHoy');
-    render(valoresResTemp.temperatura, 'temperaturaHoy');
-  },
-  reason => {
-    console.log(reason);
-  }
-);
-
-const render = (value, element) => {
-  ReactDOM.render(
-    React.createElement(Valor, { valor: value }, null),
-    document.getElementById(element)
-  );
-};
+element.onclick = movida;
---
+++ interface/src/peticionParticle.js
@@ -0,0 +1,11 @@
+import Particle from 'particle-api-js';
+var particle = new Particle();
+
+var fnPr = particle.callFunction({ deviceId: '330024001747343338333633', name: 'routest', argument: 'RL', auth: '18ac8c8ca8c8fc6d0204c1042729b5f90040dc57' });
+
+fnPr.then(
+    function(data) {
+      console.log('Function called succesfully:', data);
+    }, function(err) {
+      console.log('An error occurred:', err);
+    });
